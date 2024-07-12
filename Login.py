a651708a50bb29033229baa3089f748432572c8c
Gustavo de Carvalho <Gustavocarvalhofernandes@gmail.com>
2024-07-12 17:29:05 -0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -6,26 +6,25 @@ cursor = conn.cursor()
 
 def logar(name,password):
     nome_login= name
-    Senha_login = password
+    senha_login = password
 
     cursor.execute("SELECT senha FROM usuarios WHERE nome = ?",(nome_login,))
     try:
         senha_banco = cursor.fetchone()[0]
         
-        if senha_banco == Senha_login:
+        if senha_banco == senha_login:
             print("Login efetuado com sucesso!")
         else:
             print('senha incorreta!')
 
     except Exception as e:
-        print('Usuario ou senha incorreto!')
+        print('Usuario não encontrado!')
 
 root = ctk.CTk()
 root.geometry('400x300')
 root.resizable(False,False)
-ctk.set_default_color_theme("green")
-ctk.set_appearance_mode("green")
-
+ctk.set_default_color_theme("dark-blue")
+ctk.set_appearance_mode("dark")
 
 root.mainloop()
 
