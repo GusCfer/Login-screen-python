f92ca32300704729b616e050dbbbb9583e6578c9
Gustavo de Carvalho <Gustavocarvalhofernandes@gmail.com>
2024-07-13 09:41:15 -0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -4,6 +4,7 @@ import customtkinter as ctk
 conn = sqlite3.connect(r'C:\SQLITE\BANCOTESTE\data.db')
 cursor = conn.cursor()
 
+#definindo a função para validar usuario e senha
 def logar(name,password):
     nome_login= name
     senha_login = password
@@ -20,11 +21,15 @@ def logar(name,password):
     except Exception as e:
         print('Usuario não encontrado!')
 
+#inicação da criação da janela e configuração padrão
 root = ctk.CTk()
-root.geometry('400x300')
+root.geometry('800x600')
 root.resizable(False,False)
+#definição de tema
 ctk.set_default_color_theme("dark-blue")
 ctk.set_appearance_mode("dark")
+#inicio da definição do widgets
+
 
 root.mainloop()
 
