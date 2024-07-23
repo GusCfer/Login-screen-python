072701e9962de3cb4a384b05f793baf88786564d
Gustavo de Carvalho <Gustavocarvalhofernandes@gmail.com>
2024-07-23 09:06:04 -0300

    update

--- Login.py
+++ Login.py
@@ -28,6 +28,8 @@ def clima(namecity):
     situacao = condicoes['text']
     uconsulta = dados['last_updated']
     pais = localizacao['country']
+    estado = localizacao['region']
+
 
    #WIDGETS DO CLIMA
     #chamando a requisição para buscar imagem do clima e inserindo na tela
@@ -35,55 +37,69 @@ def clima(namecity):
     requisicaoimagem = requests.get(image)
     image_data = requisicaoimagem.content
     image_pillow = Image.open(io.BytesIO(image_data))  
-    image_ctk = ctk.CTkImage(light_image=image_pillow, size=(image_pillow.width,50))
+    image_ctk = ctk.CTkImage(light_image=image_pillow, size=(100,100))
     #chamando widgets da imagem 
     imagelabel = ctk.CTkLabel(master=root2,image=image_ctk,text='')
-    imagelabel.place(relx=0.33,rely=0.329)
+    imagelabel.place(relx=0.623,rely=0.05)
+    #divisão
+    divisao = ctk.CTkFrame(master=root2,height=400,width=6,fg_color='gray')
+    divisao.place(relx=0.68,rely=0.25)
     #chamando os dados do clima e colocando na tela
-    temp = ctk.CTkLabel(root2,text='Temperatura c°',font=('Arial',10),text_color='#6da4a4')
-    temp.place(relx=0.1,rely=0.5)
-    tempcity = ctk.CTkLabel(root2,text=temperatura,font=('Arial',10))
-    tempcity.place(relx=0.1,rely=0.56)
+    temp = ctk.CTkLabel(root2,text='Temperatura c°',font=('Calibri',20),text_color='#8651e8')
+    temp.place(relx=0.45,rely=0.25)
+    tempcity = ctk.CTkLabel(root2,text=temperatura,font=('Calibri',20))
+    tempcity.place(relx=0.45,rely=0.3)
     #EM F
-    temp2 = ctk.CTkLabel(root2,text='Temperatura f°',font=('Arial',10),text_color='#6da4a4')
-    temp2.place(relx=0.55,rely=0.5)
-    tempcity2 = ctk.CTkLabel(root2,text=temperaturaf,font=('Arial',10))
-    tempcity2.place(relx=0.55,rely=0.56)
+    temp2 = ctk.CTkLabel(root2,text='Temperatura f°',font=('Calibri',20),text_color='#8651e8')
+    temp2.place(relx=0.75,rely=0.25)
+    tempcity2 = ctk.CTkLabel(root2,text=temperaturaf,font=('Calibri',20))
+    tempcity2.place(relx=0.75,rely=0.3)
     #status
-    temp = ctk.CTkLabel(root2,text='Status',font=('Arial',10),text_color='#6da4a4')
-    temp.place(relx=0.1,rely=0.62)
-    tempcity = ctk.CTkLabel(root2,text=situacao,font=('Arial',10))
-    tempcity.place(relx=0.1,rely=0.68)
+    statusvar = ctk.CTkLabel(root2,text='Status',font=('Calibri',20),text_color='#8651e8')
+    statusvar.place(relx=0.45,rely=0.35)
+    statusvar = ctk.CTkLabel(root2,text=situacao,font=('Calibri',20))
+    statusvar.place(relx=0.45,rely=0.4)
     #country
-    temp = ctk.CTkLabel(root2,text='Pais',font=('Arial',10),text_color='#6da4a4')
-    temp.place(relx=0.55,rely=0.62)
-    tempcity = ctk.CTkLabel(root2,text=pais,font=('Arial',10))
-    tempcity.place(relx=0.55,rely=0.68)
+    temp = ctk.CTkLabel(root2,text='Pais',font=('Calibri',20),text_color='#8651e8')
+    temp.place(relx=0.75,rely=0.355)
+    tempcity = ctk.CTkLabel(root2,text=pais,font=('Calibri',20))
+    tempcity.place(relx=0.75,rely=0.4)
     #ultimaconsulta
-    temp = ctk.CTkLabel(root2,text='Ultima consulta',font=('Arial',10),text_color='#6da4a4')
-    temp.place(relx=0.1,rely=0.74)
-    tempcity = ctk.CTkLabel(root2,text=uconsulta,font=('Arial',10))
-    tempcity.place(relx=0.1,rely=0.81)
+    temp = ctk.CTkLabel(root2,text='Ultima consulta',font=('Calibri',20),text_color='#8651e8')
+    temp.place(relx=0.45,rely=0.45)
+    tempcity = ctk.CTkLabel(root2,text=uconsulta,font=('Calibri',20))
+    tempcity.place(relx=0.45,rely=0.5)
+    #ESTADO
+    estadlabel = ctk.CTkLabel(root2,text='Estado',font=('Calibri',20),text_color='#8651e8')
+    estadlabel.place(relx=0.75,rely=0.45)
+    estad = ctk.CTkLabel(root2,text=estado,font=('Calibri',20))
+    estad.place(relx=0.75,rely=0.5)
+
     
 #configuração padrão da janela
 root2= ctk.CTk()
 root2.geometry('800x600')
 root2.resizable(False,False)
 ctk.set_default_color_theme("green")
-ctk.set_appearance_mode("system")
+ctk.set_appearance_mode("dark")
+
+#canva
+frame = ctk.CTkFrame(root2,width=290,height=700,fg_color='#3d3d3d')
+frame.place(relx=0.0,rely=-0.1)
 
 #widgets padrão
-label1 = ctk.CTkLabel(root2,text='Digite a Cidade:',font=('Arial',15))
-label1.place(relx=0.23,rely=0.02)
+label1 = ctk.CTkLabel(frame,text='Digite a Cidade:',font=('Calibri',15))
+label1.place(relx=0.3,rely=0.35)
+
 #campo para entrada de texto
-selectioncity = ctk.CTkEntry(root2,width=160,height=10,placeholder_text='digite aqui!')
-selectioncity.place(relx=0.1,rely=0.13)
+selectioncity = ctk.CTkEntry(frame,width=160,height=10,placeholder_text='digite aqui!')
+selectioncity.place(relx=0.2,rely=0.4)
 selectioncity.insert(ctk.END,'')
 
 #botão para chamar as informações
-button = ctk.CTkButton(root2,width=100,height=30,text='Pesquisar',fg_color='#8651e8',
-                       command=lambda: clima(selectioncity.get()),text_color='black')
-button.place(relx=0.25,rely=0.23)
+button = ctk.CTkButton(frame,width=100,height=30,text='PESQUISAR',fg_color='#8651e8',
+                       command= lambda: clima(selectioncity.get()),text_color='black')
+button.place(relx=0.3,rely=0.45)
 
 #definindo a função para validar usuario e senha
 def logar(name,password):
@@ -114,10 +130,10 @@ root.resizable(False,False)
 ctk.set_appearance_mode("dark")
 #inicio da definição do widgets
 #imagem background da tela de login
-background = Image.open('C:\imagem\BACKGROUND.png')
+'''background = Image.open('C:\imagem\BACKGROUND.png')
 image_ctk = ctk.CTkImage(light_image=background, size=(background.width,background.height))
-backgroundlabel = ctk.CTkButton(master=root,image=image_ctk,text='')
-backgroundlabel.place(relx=0,rely=0)
+backgroundlabel = ctk.CTkLabel(master=root,image=image_ctk,text='')
+backgroundlabel.place(relx=0,rely=0)'''
 
 #configuração do canva cinza que fica na direita (frame cinza)
 graycanva = ctk.CTkFrame(root,width=290,height=700,fg_color='#3d3d3d')
@@ -134,7 +150,7 @@ ususenha= ctk.CTkEntry(graycanva,width=160,height=10,placeholder_text='Senha',sh
 ususenha.place(relx=0.23,rely=0.45)
 
 #botão de login (button)
-buttonlogin = ctk.CTkButton(graycanva,width=120,height=20,text='Entrar',fg_color='#8651e8',text_color='#333233',
+buttonlogin = ctk.CTkButton(graycanva,width=120,height=20,text='ENTRAR',fg_color='#8651e8',text_color='#333233',
                             command=lambda:logar(usuentry.get(),int(ususenha.get())))
 buttonlogin.place(relx=0.3,rely=0.5)
 
@@ -160,13 +176,13 @@ def createusu():
             cursor.execute(f"insert into usuarios(nome,senha) values('{usu}',{senha})")
             conn.commit()
             mensagem = ctk.CTkLabel(root3,text='Criado com sucesso!',font=('Calibri',15),text_color='Green')
-            mensagem.place(relx=0.33,rely=0.4)
+            mensagem.place(relx=0.2,rely=0.8)
         except Exception as e:
             mensagem = ctk.CTkLabel(root3,text='Erro!',font=('Calibri',15),text_color='Red')
-            mensagem.place(relx=0.33,rely=0.4)
+            mensagem.place(relx=0.2,rely=0.8)
             print(e)
     
-    buttoncreate = ctk.CTkButton(root3,height=20,width=70,text='Criar',text_color='Black',
+    buttoncreate = ctk.CTkButton(root3,height=20,width=70,text='CRIAR',text_color='Black',
                                  command=lambda:insertnewusu(str(newusu.get()),int(newpass.get())),fg_color='#8651e8')
     buttoncreate.place(relx=0.34,rely=0.64)
 
